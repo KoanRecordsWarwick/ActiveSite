2271e0bf2609ff1c19725ccb233d2e08307cf504
Joe Colvin <joefishbass@gmail.com>
2018-02-11 20:55:26 +0000

    Written views, models and copied templates

--- main/models.py
+++ main/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    caption = models.CharField(max_length=1000)
+
+    class Meta:
+        label = "name"
+
+class Carousel(models.Model):
+    name = models.CharField(max_length=50, default='ineedaname')
+    images = models.ManyToManyField(Image)
+
+    class Meta:
+        label = "name"
+
+class NewsItem(models.Model):
+    markdown = models.CharField(max_length=20000)
+    published_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        label = "published_at"
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=50000)
+    releases = models.ManyToManyField('Release', blank=True)
+    spotifylink = models.CharField(max_length=1000)
+    soundcloudlink = models.CharField(max_length=1000)
+    bandcamplink = models.CharField(max_length=1000)
+    applemusiclink = models.CharField(max_length=1000)
+
+    class Meta:
+        label = "name"
+
+class Release(models.Model):
+    title = models.CharField(max_length=200)
+    typevideotrack = models.CharField(max_length=50)
+    artists = models.ManyToManyField(Artist)
+    releasedate = models.DateField()
+    link = models.CharField(max_length=1000)
+
+    class Meta:
+        label = "title"
--- main/views.py
+++ main/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.template import loader, Context
+from main.models import *
+
+def index(request):
+    carousel = Carousel.objects.get(name='index'),
+    c = {
+            "images" : carousel[0].images.all()
+        }
+    return render(request, 'index.html', c)
+
+def news(request):
+    return render(request, 'news.html')
+
+def artists(request):
+    return render(request, 'artists.html')
+
+def releases(request):
+    c = {
+            "videos" : Release.objects.filter(typevideotrack='video'),
+            "tracks" : Release.objects.filter(typevideotrack='track'),
+        }
+    return render(request, 'releases.html', c)
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
 
 # Create your views here.
