29781ca5d08347c4a404ae76ef8a128ad9326b31
koan <recordskoan@gmail.com>
2018-02-12 11:30:20 +0000

    registered models with admin site

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Image)
+admin.site.register(Carousel)
+admin.site.register(NewsItem)
+admin.site.register(Artist)
+admin.site.register(Release)
 
 # Register your models here.
